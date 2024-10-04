a6f7f50594a5342f7b8fd883da0c750f81cb3fff
karthikeyankumbam <karthikeyankumbam123@gmail.com>
2024-10-04 10:56:29 +0530

    Adding multithreading Practicing code

--- src/main/java/com/design/patterns/multithreading/ThreadCoordination.java
+++ src/main/java/com/design/patterns/multithreading/ThreadCoordination.java
@@ -1,10 +1,15 @@
 package com.design.patterns.multithreading;
 
+import java.math.BigInteger;
+
 public class ThreadCoordination {
     public static void main(String[] args) {
-        Thread task = new Thread(new  BlockingTask());
-        task.start();
-        task.interrupt();
+//        Thread task = new Thread(new  BlockingTask());
+//        task.start();
+//        task.interrupt();
+        Thread thread = new Thread(new LongComputationalTask(new BigInteger("2000000"), new BigInteger("1000000")));
+        thread.start();
+        thread.interrupt();
     }
     private static  class BlockingTask implements Runnable{
 
@@ -15,6 +20,31 @@ public class ThreadCoordination {
             }catch (InterruptedException e){
                 System.out.println("Exiting the blocking thread");
             }
+        }
+    }
+    private static class LongComputationalTask implements Runnable{
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputationalTask(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(base+"^"+power+" = "+pow(base,power));
+        }
+        private BigInteger pow(BigInteger base, BigInteger power){
+            BigInteger result=BigInteger.ONE;
+            for(BigInteger i=BigInteger.ZERO;i.compareTo(power)!=0;i=i.add(BigInteger.ONE)){
+                if(Thread.currentThread().isInterrupted()){
+                    System.out.println("Prematurely Interrupted Communication");
+                    return BigInteger.ZERO;
+                }
+                result = result.multiply(base);
+            }
+            return result;
         }
     }
 }
